d3f7d505a7e3a525c8039ff6dc2112aee9db3ba6
AlecGiven <78773178+AlecGiven@users.noreply.github.com>
2021-05-02 23:53:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function login_user() 
+{
+	let username = document.getElementById("username").value;
+	let password = document.getElementById("password").value;
+
+	if (username == "cool") {
+		if (password == "password") {
+			document.location.href = "https://000616342.codepen.website/grade_view.html";
+		} else {
+			alert("password is incorrect!");
+		}
+	} else {
+		alert("username is incorrect!");
+	}
+}
+
+function toggle_page_view(view) {
+	if (view = "grade_view") {
+		document.getElementById("grade_view").style.visibility = "hidden";
+		document.getElementById("add_grade").style.visibility = "visible";
+	} else {
+		document.getElementById("grade_view").style.visibility = "visible";
+		document.getElementById("add_grade").style.visibility = "hidden";
+	}
+}
+
+document.getElementById("add_grade").style.visibility = "hidden";
+
+function add_grade() {
+	let name = document.getElementById("name").value;
+	let value = Number(document.getElementById("value").value);
+	
+	if (!isNaN(value)) {
+		if (value >= 0 && value <= 100) {
+				document.getElementById('grade_list').innerHTML += "Name: " + name + " Grade: " + value + "<br />";
+				document.getElementById("grade_view").style.visibility = "visible";
+				document.getElementById("add_grade").style.visibility = "hidden";
+			} else {
+				alert('Error! Grade value is either too high or too low! (Range is 0-100)');
+			}
+		} else {
+			alert('Error! Grade value is invalid!');
+		}
+}
+
+function go_back() {
+	document.getElementById("grade_view").style.visibility = "visible";
+	document.getElementById("add_grade").style.visibility = "hidden";
+}
